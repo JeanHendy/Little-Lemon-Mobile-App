ad59fbe95b80e148b86b01dc55e1002b60c868cb
Jean Hendy <jeanyikikh@gmail.com>
2024-04-11 13:23:24 +0200

    added and configure text input boxes, handled the Keyboard mode.

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import { View, StyleSheet } from "react-native";
-
 import LittleLemonHeader from "./components/LittleLemonHeader";
 import LittleLemonFooter from "./components/LittleLemonFooter";
 import MenuItems from "./components/MenuItems";
+import FeedbackForm from "./components/FeedbackForm.js";
+import LoginScreen from "./components/LoginScreen.js";
+
 
 export default function App() {
   return (
     <>
       <View style={styles.container}>
         <LittleLemonHeader />
-        <MenuItems />
+        <FeedbackForm />
       </View>
       <View style={styles.footerContainer}>
         <LittleLemonFooter />
